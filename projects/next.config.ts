3c83be8b16de4032ca470f06c578d9429cf7c6a5
zhangtao <git.vistaflyer.com>
2026-06-01 16:10:38 +0800

    update

--- projects/next.config.ts
+++ projects/next.config.ts
@@ -9,16 +9,7 @@ const nextConfig: NextConfig = {
         hostname: '**',
       },
     ],
-  },
-  async redirects() {
-    return [
-      {
-        source: '/ads.txt',
-        destination: 'https://srv.adstxtmanager.com/19390/enjoy4game.com',
-        permanent: true, // 启用 301 重定向
-      },
-    ]
-  },
+  }
 };
 
 export default nextConfig;
